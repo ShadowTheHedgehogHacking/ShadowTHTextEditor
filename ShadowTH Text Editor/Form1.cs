ad83f7bf552684254af86cc6dfb6badf4661e08b
dreamsyntax <dreamsyntax@gmail.com>
2019-11-08 21:25:01 -0700

    create simple struct; parse SubtitleTable and strings

--- ShadowTH Text Editor/Form1.cs
+++ ShadowTH Text Editor/Form1.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ShadowTH_Text_Editor
 {
     public partial class Form1 : Form
     {
+        private Byte[] originalFile;
+        private List<SubtitleTableEntry> subtitleTable;
+        private List<String> subtitleList;
+
         public Form1()
         {
             InitializeComponent();
@@ -32,6 +32,62 @@ namespace ShadowTH_Text_Editor
                 0x0C: Subtitle active time - Retain original value
                 0x10: Subtitle ID - Retain original value
              */
+
+            originalFile = File.ReadAllBytes("X:\\Movie_EN.fnt");
+
+            // TODO: Determine if reverse endian is necessary, Seems to vary per .fnt?? Need to double check
+            int numberOfSubtitles = BitConverter.ToInt32(originalFile, 0);
+            //int numberOfSubtitles = BinaryPrimitives.ReverseEndianness(BitConverter.ToInt32(originalFile, 0));
+
+            int subtitleTableEntryStructSize = 0x14;
+            int positionIndex = 4; //skip header
+
+            subtitleTable = new List<SubtitleTableEntry>();
+            subtitleList = new List<String>();
+
+            //read table entries
+            for (int i = 0; i < numberOfSubtitles; i++)
+            {
+                SubtitleTableEntry entry = new SubtitleTableEntry
+                {
+                    startingPosition = BitConverter.ToInt32(originalFile, positionIndex),
+                    externalAddress = BitConverter.ToInt32(originalFile, positionIndex + 4),
+                    textType = BitConverter.ToInt32(originalFile, positionIndex + 8),
+                    subtitleActiveTime = BitConverter.ToInt32(originalFile, positionIndex + 12),
+                    subtitleId = BitConverter.ToInt32(originalFile, positionIndex + 16)
+                };
+                //Save entry in a table holding entries which map to UTF-16 entries below
+                subtitleTable.Add(entry);
+                positionIndex += subtitleTableEntryStructSize;
+            }
+
+            //read UTF-16 entries
+            for (int i = 0; i < numberOfSubtitles; i++)
+            {
+                int subtitleLength = calculateSize(i);
+                String test = Encoding.Unicode.GetString(originalFile, positionIndex, subtitleLength);
+                subtitleList.Add(test);
+                positionIndex += subtitleLength;
+            }
         }
+
+        private int calculateSize(int entryIndex) {
+            // if last subtitleTable entry, size is originalFilesize - entry index
+            if (entryIndex == subtitleTable.Count - 1) { 
+                return originalFile.Length - subtitleTable[entryIndex].startingPosition;
+            }
+
+            //otherwise calculate based on next entry in list
+            return subtitleTable[entryIndex + 1].startingPosition - subtitleTable[entryIndex].startingPosition;
+        }
+
+        struct SubtitleTableEntry
+        {
+            public int startingPosition;
+            public int externalAddress;
+            public int textType; //enumify later if going to make this modifiable
+            public int subtitleActiveTime;
+            public int subtitleId;
+        };
     }
 }
