70a9e551510620a371579085e2c8fac1ae652a11
TheHatedGravity <no@email.com>
2019-11-10 16:10:07 +1300

    General prettification

--- ShadowTH Text Editor/Form1.Designer.cs
+++ ShadowTH Text Editor/Form1.Designer.cs
@@ -40,18 +40,21 @@
             // 
             // subtitleTextBox
             // 
-            this.subtitleTextBox.BackColor = System.Drawing.Color.Black;
-            this.subtitleTextBox.ForeColor = System.Drawing.Color.Yellow;
+            this.subtitleTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.subtitleTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.subtitleTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.subtitleTextBox.Location = new System.Drawing.Point(165, 23);
             this.subtitleTextBox.Multiline = true;
             this.subtitleTextBox.Name = "subtitleTextBox";
-            this.subtitleTextBox.Size = new System.Drawing.Size(269, 290);
+            this.subtitleTextBox.Size = new System.Drawing.Size(269, 288);
             this.subtitleTextBox.TabIndex = 0;
             // 
             // buttonSave
             // 
-            this.buttonSave.BackColor = System.Drawing.Color.Black;
-            this.buttonSave.ForeColor = System.Drawing.Color.Yellow;
+            this.buttonSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.buttonSave.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
             this.buttonSave.Location = new System.Drawing.Point(307, 323);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(113, 42);
@@ -62,8 +65,10 @@
             // 
             // buttonOpen
             // 
-            this.buttonOpen.BackColor = System.Drawing.Color.Black;
-            this.buttonOpen.ForeColor = System.Drawing.Color.Yellow;
+            this.buttonOpen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.buttonOpen.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.buttonOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOpen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
             this.buttonOpen.Location = new System.Drawing.Point(180, 323);
             this.buttonOpen.Name = "buttonOpen";
             this.buttonOpen.Size = new System.Drawing.Size(102, 42);
@@ -74,19 +79,22 @@
             // 
             // subtitleTable_ListBox
             // 
-            this.subtitleTable_ListBox.BackColor = System.Drawing.Color.Black;
-            this.subtitleTable_ListBox.ForeColor = System.Drawing.Color.Yellow;
+            this.subtitleTable_ListBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.subtitleTable_ListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.subtitleTable_ListBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.subtitleTable_ListBox.FormattingEnabled = true;
             this.subtitleTable_ListBox.Location = new System.Drawing.Point(12, 23);
             this.subtitleTable_ListBox.Name = "subtitleTable_ListBox";
-            this.subtitleTable_ListBox.Size = new System.Drawing.Size(134, 290);
+            this.subtitleTable_ListBox.Size = new System.Drawing.Size(134, 288);
             this.subtitleTable_ListBox.TabIndex = 3;
             this.subtitleTable_ListBox.SelectedIndexChanged += new System.EventHandler(this.subtitleTable_ListBox_SelectedIndexChanged);
             // 
             // aboutButton
             // 
-            this.aboutButton.BackColor = System.Drawing.Color.Black;
-            this.aboutButton.ForeColor = System.Drawing.Color.Yellow;
+            this.aboutButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.aboutButton.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.aboutButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.aboutButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
             this.aboutButton.Location = new System.Drawing.Point(26, 323);
             this.aboutButton.Name = "aboutButton";
             this.aboutButton.Size = new System.Drawing.Size(102, 42);
@@ -110,15 +118,18 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
             this.ClientSize = new System.Drawing.Size(446, 377);
             this.Controls.Add(this.aboutButton);
             this.Controls.Add(this.subtitleTable_ListBox);
             this.Controls.Add(this.buttonOpen);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.subtitleTextBox);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(462, 416);
+            this.MinimumSize = new System.Drawing.Size(462, 416);
             this.Name = "Form1";
             this.Text = "ShadowTH Text Editor Prototype Edition";
             this.ResumeLayout(false);
