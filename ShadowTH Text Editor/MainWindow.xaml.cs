950f0b83f51340abdf79e892fc5d9f488dd910f3
dreamsyntax <dreamsyntax@gmail.com>
2019-12-08 19:30:07 -0700

    parse fonts folder, add button and listbox

--- ShadowTH Text Editor/MainWindow.xaml.cs
+++ ShadowTH Text Editor/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ShadowFNT.Structures;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +20,37 @@ namespace ShadowTH_Text_Editor {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        List<FNT> openedFnts;
+
         public MainWindow() {
             InitializeComponent();
+            openedFnts = new List<FNT>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            if (dialog.ShowDialog() == false) {
+                return;
+            }
+            if (!dialog.SelectedPath.EndsWith("fonts")) {
+                MessageBox.Show("Pick the 'fonts' folder extracted from Shadow The Hedgehog", "Try Again");
+                return;
+            }
+            clearData();
+            string originalPath = dialog.SelectedPath;
+            string[] foundFnts = Directory.GetFiles(dialog.SelectedPath, "*.fnt", SearchOption.AllDirectories);
+            for (int i = 0; i < foundFnts.Length; i++) {
+                byte[] readFile = File.ReadAllBytes(foundFnts[i]);
+                FNT newFnt = FNT.ParseFNTFile(foundFnts[i], ref readFile);
+                openedFnts.Add(newFnt);
+                ListBox_OpenedFNTS.Items.Add(foundFnts[i].Split(dialog.SelectedPath + '\\')[1]);
+            }
+        }
+
+        private void clearData() {
+            openedFnts = new List<FNT>();
+            ListBox_OpenedFNTS.Items.Clear();
+
         }
     }
 }
